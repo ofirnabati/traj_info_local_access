f31f0ba4037d4f9e9964dedc3d6fa3e87de89c67
Ofir Nabati <>
2023-03-23 12:52:19 +0200

    more updates for dws

--- barl/envs/reacher.py
+++ barl/envs/reacher.py
@@ -1,4 +1,6 @@
 import os
+
+import ipdb
 import numpy as np
 import tensorflow as tf
 from gym import utils, spaces
@@ -7,6 +9,7 @@ from gym.envs.mujoco import mujoco_env
 
 class BACReacherEnv(mujoco_env.MujocoEnv, utils.EzPickle):
     def __init__(self, tight=False):
+        ipdb.set_trace()
         utils.EzPickle.__init__(self)
         self.horizon = 50
         self.periodic_dimensions = [0, 1]
